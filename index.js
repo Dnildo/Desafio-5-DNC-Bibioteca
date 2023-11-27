6d152475eceea12be7092c939631bc8d87f955af
Denildo mendes <93888677+Dnildo@users.noreply.github.com>
2023-11-27 10:21:34 -0300

    Projeto-5-DNC

---
+++ index.js
@@ -0,0 +1,24 @@
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+if (process.env.NODE_ENV !== 'test'){
+    const swaggerFile = require('./swagger/swagger_output.json');
+    app.get('/', (req, res) => { /*swagger.ignore = true */ res.redirect('/doc'); });
+    app.use('/doc', authDocProducao, swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
+    }
+    
+    routes(app);
+    
+
+if (process.env.NODE_ENV !== 'test') {
+    const PORT = process.env.PORT || 4000;
+    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+}
