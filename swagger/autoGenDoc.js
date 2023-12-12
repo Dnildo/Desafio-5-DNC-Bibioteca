f0b827c584be2f298676d7e4fc45b684008d9601
Denildo mendes <93888677+Dnildo@users.noreply.github.com>
2023-12-11 22:21:01 -0300

    Update autoGenDoc.js

--- swagger/autoGenDoc.js
+++ swagger/autoGenDoc.js
@@ -16,7 +16,7 @@ let doc = {
     },
     servers: [
         {
-            url: "http://localhost:5500/",
+            url: "http://localhost:27017/",
             description: "Servidor localhost."
         },
         {
