5465617593f9fef7d6e196859482d1caa7fb90ad
Denildo mendes <93888677+Dnildo@users.noreply.github.com>
2024-01-26 08:03:44 -0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+    port: 27017,
+  },
+
+  preview: {
+    port: 27017,
+  },
+});
